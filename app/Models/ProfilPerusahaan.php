3658298a815c9fe3317676b4ff57b6876cfeeb10
Fadhil <88182212+FadhilHere@users.noreply.github.com>
2025-12-16 17:09:38 +0700

    fix typo nama tabel dan PK

--- app/Models/ProfilPerusahaan.php
+++ app/Models/ProfilPerusahaan.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProfilPerusahaan extends Model
 {
+    use HasFactory;
+
     public $incrementing = true;
     public $timestamps = true;
-    protected $table = 'profildokter';
+    protected $table = 'profilPerusahaan';
 
-    protected $primaryKey = 'idDokter';
+    protected $primaryKey = 'idProfil';
 
     protected $fillable = [
         'visi',
